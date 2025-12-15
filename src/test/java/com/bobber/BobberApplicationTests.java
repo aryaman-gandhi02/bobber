2515317733b100ff466a1ccc24e94c1457dacc6f
Aryaman <aryaman.powerranger@gmail.com>
2025-12-16 02:04:35 +0530

    Initial commit

---
+++ src/test/java/com/bobber/BobberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
